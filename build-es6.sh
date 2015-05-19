988964710b074e672b7b1ddd34c260974efd5c49
rgrannell1 <r.grannell2@gmail.com>
2015-05-19 15:41:32 +0100

    adding main manager

--- build-es6.sh
+++ build-es6.sh
@@ -1,43 +1,8 @@
 #!/usr/bin/env sh
 
-for file in node_modules/engram/es6/*.es6
-do
-
-	filename="$(
-		echo $file |
-		sed "s/\.es6/\.js/" |
-		sed "s/es6//g"
-
-	)"
-
-	echo $file "->" $filename
-
-	babel $file --out-file $filename
-
-done
 
 
 
 
-
-for file in bin/*.es6
-do
-
-	filename="$(
-		echo $file |
-		sed "s/\.es6/\.js/" |
-		sed "s/es6//g"
-
-	)"
-
-	echo $file "->" $filename
-
-	babel $file --out-file $filename
-
-done
-
-
-
-
-
-echo "done."
+babel node_modules/engram/es6/ --watch --out-dir node_modules/engram/ &
+babel bin/                     --watch --out-dir bin/ &
