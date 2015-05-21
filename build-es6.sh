87b16c7745a510a713f28e78221f1e27ecbeee88
rgrannell1 <r.grannell2@gmail.com>
2015-05-21 03:13:59 +0100

    improved logging

--- build-es6.sh
+++ build-es6.sh
@@ -4,5 +4,5 @@
 
 
 
-babel node_modules/engram/es6/ --watch --out-dir node_modules/engram/ &
-babel bin/                     --watch --out-dir bin/ &
+babel node_modules/engram/es6/ --watch --out-dir node_modules/engram/
+babel bin/                     --watch --out-dir bin/
