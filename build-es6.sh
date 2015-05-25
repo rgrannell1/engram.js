38969588672a405ed59a4e8eec74892385f13c02
rgrannell1 <r.grannell2@gmail.com>
2015-05-25 02:33:36 +0100

    added tests

--- build-es6.sh
+++ build-es6.sh
@@ -4,6 +4,7 @@
 
 
 
-babel node_modules/engram/es6/ --watch --out-dir node_modules/engram/
-babel public/javascript/src/   --watch --out-dir public/javascript/lib
-babel bin/                     --watch --out-dir bin/
+babel node_modules/engram/test/es6 --watch --out-dir node_modules/engram/test &
+babel node_modules/engram/es6/     --watch --out-dir node_modules/engram/     &
+babel public/javascript/src/       --watch --out-dir public/javascript/lib    &
+babel bin/                         --watch --out-dir bin/                     &
