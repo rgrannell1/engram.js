c576e087141e2367f81e0e6b29e9b3ce92c94108
rgrannell1 <r.grannell2@gmail.com>
2015-05-25 21:47:47 +0100

    fixed undefined bug

--- bin/docopt-engram.js
+++ bin/docopt-engram.js
@@ -1,1 +1,12 @@
+#!/usr/bin/env node
 "use strict";
+
+var doc = "\n\nUsage:\n    engram --port=<port>\n    engram (-h | --help | --version)\n\nVersion:\nv0.1.0\n\nDescription:\n\nOptions:\n    --port=<port>    [default: 5000] the port to run the server on.\n";
+
+var docopt = require("docopt").docopt;
+var engram = require("engram/engram");
+var args = docopt(doc);
+
+engram({
+    port: args["--port"]
+});
