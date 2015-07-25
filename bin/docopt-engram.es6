77519694b87c7a16d70b95f9785b112ef00d90b8
rgrannell1 <r.grannell2@gmail.com>
2015-07-25 23:07:35 +0100

    added certificate generation

--- bin/docopt-engram.es6
+++ bin/docopt-engram.es6
@@ -11,18 +11,23 @@
 var doc = `
 
 Usage:
-    engram --port=<port> --dbpath=<dbpath>
+    engram [--port=<port>] [--dbpath=<dbpath>] [--keypath=<path>] [--certpath=<path>]
+    engram certs                               [--keypath=<path>] [--certpath=<path>]
     engram wipe logs
     engram wipe db
     engram (-h | --help | --version)
 
 Version:
-v0.1.0
+	v0.1.0
 
 Description:
 
+
 Options:
-    --port=<port>    [default: 5000] the port to run the server on.
+    --port=<port>        The port to run the server on [default: 5000].
+    --dbpath=<dbpath>    The path to which the database should be saved [default: /data/engram.db].
+    --keypath=<path>     [default: certs/key.pem].
+    --certpath=<path>    [default: certs/cert.pem].
 `
 
 
@@ -37,9 +42,16 @@ var args   = docopt(doc)
 
 
 engram({
-	port:   parseInt(args['--port'], 10),
-	dbpath: args['--dbpath'],
-	wipe:   args.wipe,
-	db:     args.db,
-	logs:   args.logs
+	port:     parseInt(args['--port'], 10),
+
+	dbpath:   args['--dbpath'],
+	keypath:  args['--keypath'],
+	certpath: args['--certpath'],
+
+
+	wipe:     args.wipe,
+	certs:    args.certs,
+
+	db:       args.db,
+	logs:     args.logs
 })
--- bin/docopt-engram.js
+++ bin/docopt-engram.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 "use strict";
 
-var doc = "\n\nUsage:\n    engram --port=<port> --dbpath=<dbpath>\n    engram wipe logs\n    engram wipe db\n    engram (-h | --help | --version)\n\nVersion:\nv0.1.0\n\nDescription:\n\nOptions:\n    --port=<port>    [default: 5000] the port to run the server on.\n";
+var doc = "\n\nUsage:\n    engram [--port=<port>] [--dbpath=<dbpath>] [--keypath=<path>] [--certpath=<path>]\n    engram certs                               [--keypath=<path>] [--certpath=<path>]\n    engram wipe logs\n    engram wipe db\n    engram (-h | --help | --version)\n\nVersion:\n\tv0.1.0\n\nDescription:\n\n\nOptions:\n    --port=<port>        The port to run the server on [default: 5000].\n    --dbpath=<dbpath>    The path to which the database should be saved [default: /data/engram.db].\n    --keypath=<path>     [default: certs/key.pem].\n    --certpath=<path>    [default: certs/cert.pem].\n";
 
 var docopt = require("docopt").docopt;
 var engram = require("engram/es5/engram");
 var args = docopt(doc);
 
 engram({
-    port: parseInt(args["--port"], 10),
-    dbpath: args["--dbpath"],
-    wipe: args.wipe,
-    db: args.db,
-    logs: args.logs
+  port: parseInt(args["--port"], 10),
+
+  dbpath: args["--dbpath"],
+  keypath: args["--keypath"],
+  certpath: args["--certpath"],
+
+  wipe: args.wipe,
+  certs: args.certs,
+
+  db: args.db,
+  logs: args.logs
 });
