520921efb84f97c349dd5c59de76abd6a48f2ccd
rgrannell1 <r.grannell2@gmail.com>
2015-09-27 21:45:44 +0100

    made makefile clearer

--- public/javascript/es6/main.js
+++ public/javascript/es6/main.js
@@ -11,12 +11,7 @@ var messages  = require('./messages')
 var commons   = require('./commons')
 var rest      = require('./rest')
 
-
-
-
-
 var Bookmark  = require('./model/bookmark')
-
 
 
 
@@ -30,3 +25,27 @@ window.ENGRAM.constants = constants
 window.ENGRAM.messages  = messages
 window.ENGRAM.commons   = commons
 window.ENGRAM.rest      = rest
+
+
+
+
+
+console.log('main: loaded.')
+
+
+
+
+var elem = Bookmark({
+	url:          'http://mithril.js.org/mithril.mount.html',
+	displayTitle: 'Mount',
+	hosturl:      'http://mithril.js.org',
+	hostname:     'mithril',
+	date:         (new Date( )).getTime( ),
+	bookmarkId:   '0'
+})
+
+
+
+
+
+m.mount( document.body, elem )
